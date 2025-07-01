9fdb7c58b0164701920b7791f1c6a939cad3df01
CHIRAYU-365 <CHIRAYUJAIN362005@GMAIL.COM>
2025-07-01 12:32:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def dashboard():
     return render_template("dashboard.html", labels=labels, predictions=predictions, views=views)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
